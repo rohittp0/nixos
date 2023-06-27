dc76a843a8e6b5254bef2bd76dc9c7db4d81f810
sinanmohd <sinan@firemail.cc>
2023-06-27 18:04:30 +0530

    repo: initial commit

---
+++ configuration.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # boot
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  # netwroking
+  networking = {
+    hostName = "cez";
+    wireless.iwd.enable = true;
+  };
+  time.timeZone = "Asia/Kolkata";
+
+  # sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # users
+  users.users.sinan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      git
+      tmux
+      w3m
+      neofetch
+    ];
+  };
+
+  # system
+  environment.systemPackages = with pkgs; [
+    htop
+    curl
+    neovim
+    wget
+  ];
+  system.stateVersion = "23.05";
+}
