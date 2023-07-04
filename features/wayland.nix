06e99acc73439dd7822a00f7ddb70c350c2343dd
sinanmohd <pcmsinan@gmail.com>
2023-07-04 16:47:52 +0530

    cleaup: code style

--- features/wayland.nix
+++ features/wayland.nix
@@ -7,8 +7,7 @@ let
     destination = "/bin/dbus-xdp-environment";
     executable = true;
 
-    text =
-      ''
+    text = ''
         dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
         systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
         systemctl --user start pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
