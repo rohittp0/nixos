420bcd1583ee3771faf6df093ee69f5d1b93c2ff
sinanmohd <sinan@firemail.cc>
2023-08-05 10:55:09 +0530

    wayland: get rid dconf, gsettings gtk themeing
    GTK_THEME environment variable is more simpler and cleaner
    https://git.sinanmohd.com/dots/commit/?id=488071a8da65e8f5a10e5f41321e3303cbb12b08

--- features/wayland.nix
+++ features/wayland.nix
@@ -1,24 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  # gtk theming: https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
-  # for gsettings to work, we need to tell it where the schemas are
-  # using the XDG_DATA_DIR environment variable
-  configure-gtk = pkgs.writeTextFile {
-      name = "configure-gtk";
-      destination = "/bin/configure-gtk";
-      executable = true;
-      text = let
-        schema = pkgs.gsettings-desktop-schemas;
-        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-      in
-        ''
-          export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-          gnome_schema=org.gnome.desktop.interface
-          gsettings set $gnome_schema gtk-theme 'Dracula'
-        '';
-  };
-in
 {
   # pkgs
   environment.systemPackages = with pkgs; [
@@ -47,9 +28,6 @@ in
     wlr-randr
     tor-browser-bundle-bin
     wtype
-    configure-gtk
-    dracula-theme # gtk theme
-    glib # gsettings
   ];
 
   # font
@@ -71,7 +49,6 @@ in
 
   services.dbus.enable = true;
   programs = {
-    dconf.enable = true;
     xwayland.enable = true;
     gnupg.agent = {
       enable = true;
