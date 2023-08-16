fb12b8877d0b54e7235dc52e6ecf12165ad892b4
sinanmohd <sinan@firemail.cc>
2023-08-16 19:12:26 +0530

    wayland: bump dwl version, freezshot support

--- overlays/dwl-sinan.nix
+++ overlays/dwl-sinan.nix
@@ -6,8 +6,8 @@ final: prev:
       pname = prevAttrs.pname + "-sinan";
       src = prev.fetchgit {
         url = "https://git.sinanmohd.com/dwl";
-        rev = "f708547efb0b3afe4149f6eb7bcc685fc39f351a";
-        sha256 = "04rb90hasicm4aj51403hjxyyszm87qiqz6phrjy3364vkqvrx3c";
+        rev = "cadde97ee92381bfc2222bf18cd876526ab8c948";
+        hash = "sha256-H2HgOQnQ5Eltz8/OVpKlS2U89aaivg5t63Ie4fembw8=";
       };
     }
   );
