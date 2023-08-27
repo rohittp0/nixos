d01183524b252704385415dee8cd6c7b6ec67cf8
sinanmohd <sinan@firemail.cc>
2023-08-27 21:23:10 +0530

    modules/dev: add language servers

--- features/development.nix
+++ features/development.nix
@@ -2,8 +2,13 @@
 
 {
   environment.systemPackages = with pkgs; [
+    nodePackages.bash-language-server
+    nil
+    ccls
+    lua-language-server
     man-pages
     man-pages-posix
   ];
+
   documentation.dev.enable = true;
 }
