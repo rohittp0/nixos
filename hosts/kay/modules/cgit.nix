31845c27ca0f35efacae25a034b3d36756f3a7ae
sinanmohd <sinan@firemail.cc>
2023-10-10 07:12:00 +0530

    kay/cgit: update ui options

--- hosts/kay/modules/cgit.nix
+++ hosts/kay/modules/cgit.nix
@@ -5,6 +5,7 @@ let
   user = config.userdata.user;
 in
 {
+  environment.systemPackages = with pkgs; [ luajitPackages.luaossl lua52Packages.luaossl ];
   services = {
     nginx.virtualHosts."git.${domain}" = {
       forceSSL = true;
@@ -18,10 +19,16 @@ in
         project-list = "/var/lib/git/project.list";
         remove-suffix = 1;
         enable-commit-graph = 1;
-        root-title = "${user}'s git server";
+        root-title = "${user}'s git repository";
         root-desc = "how do i learn github anon";
         source-filter = "${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py";
+        about-filter = "${pkgs.cgit}/lib/cgit/filters/about-formatting.sh";
+        readme = ":README.md";
+        footer = "";
+        enable-blame = 1;
         clone-url = "https://git.${domain}/$CGIT_REPO_URL";
+        enable-log-filecount = 1;
+        enable-log-linecount = 1;
       };
     };
   };
