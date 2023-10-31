95c14caec5d5b795317e968b4073d774cb911fdc
sinanmohd <sinan@firemail.cc>
2023-10-31 19:20:14 +0530

    userdata: clean up

--- modules/userdata.nix
+++ modules/userdata.nix
@@ -29,7 +29,7 @@ in
       type = types.listOf types.str;
       description = mdDoc "Owner's public ssh keys";
       default = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCeMXhkjm9CabbA/1xdtP9bvFEm8pVXPk66NmI9/VvQ sinan@veu"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCeMXhkjm9CabbA/1xdtP9bvFEm8pVXPk66NmI9/VvQ sinan@vex"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8LnyOuPmtKRqAZeHueNN4kfYvpRQVwCivSTq+SZvDU sinan@cez"
       ];
     };
