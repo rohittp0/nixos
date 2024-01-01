ef3e9e0894b9ba27771606d6a4a98a1c00747944
sinanmohd <sinan@sinanmohd.com>
2024-01-01 19:07:06 +0530

    kay/network/he: init routing table

--- hosts/kay/modules/hurricane.nix
+++ hosts/kay/modules/hurricane.nix
@@ -4,22 +4,65 @@ let
   iface = "hurricane";
   remote = "216.218.221.42";
   address = "2001:470:35:72a::2";
+  gateway = "2001:470:35:72a::1";
   prefixLength = 64;
+  prefix = "2001:470:35:72a::/${toString prefixLength}";
 in
 {
-  networking.sits.${iface} = {
-    inherit remote;
-    ttl = 225;
+  networking = {
+    sits.${iface} = {
+      inherit remote;
+      ttl = 225;
+    };
+    interfaces.${iface}.ipv6.addresses = [{
+      inherit prefixLength address;
+    }];
+
+    iproute2 = {
+      enable = true;
+      rttablesExtraConfig = "200 hurricane";
+    };
   };
-  networking.interfaces.${iface}.ipv6.addresses = [{
-    inherit prefixLength address;
-  }];
 
   sops.secrets = {
     "hurricane/username" = {};
     "hurricane/update_key" = {};
     "hurricane/tunnel_id" = {};
   };
+
+  systemd.services."network-route-${iface}" = {
+    description = "Routing configuration of ${iface}";
+    wantedBy = [
+      "network-setup.service"
+      "network.target"
+    ];
+    before = [ "network-setup.service" ];
+    bindsTo = [ "network-addresses-hurricane.service" ];
+    after = [ "network-pre.target" "network-addresses-hurricane.service" ];
+    # restart rather than stop+start this unit to prevent the
+    # network from dying during switch-to-configuration.
+    stopIfChanged = false;
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    path = [ pkgs.iproute2 ];
+    script = ''
+      echo -n "adding route ${prefix}... "
+
+      ip -6 rule add from ${prefix} table hurricane || exit 1
+      ip -6 route add default via ${gateway} dev hurricane table hurricane || exit 1
+    '';
+    preStop = ''
+      echo -n "deleting route $prefix... "
+
+      ip -6 route del default via ${gateway} dev hurricane table hurricane || exit 1
+      ip -6 rule del from ${prefix} table hurricane || exit 1
+    '';
+  };
+
 
   services.pppd.script."02-${iface}" = {
     runtimeInputs = with pkgs; [ curl coreutils iproute2 ];
