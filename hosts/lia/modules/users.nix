cee136a92978a0c496241ba66b6dac28a0640ffb
sinanmohd <sinan@sinanmohd.com>
2024-02-05 13:21:54 +0530

    lia/users/rohit: add ssh key

--- hosts/lia/modules/users.nix
+++ hosts/lia/modules/users.nix
@@ -2,5 +2,8 @@
   users.users."rohit" = {
     isNormalUser = true;
     packages = with pkgs; [ git ];
+
+    openssh.authorizedKeys.keys =
+      [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZcWF1zVyxsCdZ/j+h+RlHZlyhgY2Bky03847bxFNSH rohit@victus" ];
   };
 }
